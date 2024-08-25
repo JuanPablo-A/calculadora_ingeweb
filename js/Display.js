699d27625f06bcff298daf570a105ef5df5929f9
JuanPablo-A <jpablo.arango1@udea.edu.co>
2024-08-25 16:40:23 -0500

    docs: :memo: Add description in the most important code snippets

--- js/Display.js
+++ js/Display.js
@@ -1,4 +1,10 @@
 class Display {
+
+    /**
+     * Constructor de la clase Display.
+     * @param {HTMLElement} displayValorAnterior - El elemento que muestra el valor anterior.
+     * @param {HTMLElement} displayValorActual - El elemento que muestra el valor actual.
+    */
     constructor(displayValorAnterior, displayValorActual){
         /* Elemento que se quiere modificar */
         this.displayValorAnterior = displayValorAnterior;
@@ -12,7 +18,7 @@ class Display {
         this.valorAnterior = '';
         this.tipoOperacion = undefined;
 
-        /* Mapa */
+        /* Mapear las operaciones*/
         this.signos = {
             sumar: '+',
             restar: '-',
@@ -21,11 +27,15 @@ class Display {
         }
     }
 
+    /* Funciones */
+
+    /* Borra el ultimo numero que se coloco */
     borrar(){
         this.valorActual = this.valorActual.toString().slice(0,-1);
         this.imprimirValores();
     }
 
+    /* Limpia completamente el display */
     borrarTodo(){
         this.valorActual = '';
         this.valorAnterior = '';
@@ -34,17 +44,30 @@ class Display {
         this.imprimirValores();
     }
 
+    /**
+     * Muestra el resultado de la operación en el display.
+     * @param {int} numero - El numero que se quiere mostrar en el display.
+     * Verifica si el numero es un punto y si el display ya contiene un punto antes de agregarlo.
+     * Luego, convierte en string concatena el numero al valor actual y llama a `imprimirValores` para actualizar el display.
+    */
     agregarNumero(numero){
         if(numero === '.' && this.valorActual.includes('.')) return;
         this.valorActual = this.valorActual.toString() + numero.toString();
         this.imprimirValores();
     }
 
+    /* Imprime los valores en el display */
     imprimirValores(){
         this.displayValorActual.textContent = this.valorActual;
         this.displayValorAnterior.textContent = `${this.valorAnterior} ${this.signos[this.tipoOperacion] || ''}`;
     }
 
+    /**
+     * Realiza la operación indicada y actualiza los valores del display.
+     * @param {string} tipo - El tipo de operación a realizar ('sumar','restar','multiplicar','dividir').
+     * Verifica si la operación actual no es 'igual' y, si es así, llama a la función `calcular` para realizar la operación pendiente.
+     * Actualiza el tipo de operación, el valor anterior y el valor actual.
+    */
     computar(tipo){
         this.tipoOperacion !== 'igual' && this.calcular();
         this.tipoOperacion = tipo;
@@ -54,6 +77,9 @@ class Display {
         this.imprimirValores();        
     }
 
+    /**
+     * Realiza la operación pendiente y actualiza el valor actual con el resultado.
+    */
     calcular(){
         if(this.valorAnterior === '') return;
         const valorAnterior = parseFloat(this.valorAnterior);
--- js/index.js
+++ js/index.js
@@ -1,3 +1,6 @@
+/*
+    Inicializamos las variables que vamos a utilizar en la calculadora.
+*/
 const displayValorAnterior = document.getElementById('data-operador-1');
 const displayValorActual = document.getElementById('data-operador-2');
 const numeros = document.querySelectorAll('.btn-numero');
@@ -6,10 +9,10 @@ const borrar = document.querySelectorAll('.btn-borrar');
 
 const display = new Display(displayValorAnterior, displayValorActual);
 
+/* Agregar un numero y un operador cada que se da click en los respectivos botones */
 numeros.forEach(boton => {
     boton.addEventListener('click', () => display.agregarNumero(boton.innerHTML));
 })
-
 
 operadores.forEach(boton => {
     boton.addEventListener('click', () => display.computar(boton.value));
